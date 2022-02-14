1230442362882b8a85b543b10c0b51473954f34b
Aapeli <git@aapelivuorinen.com>
2022-02-14 13:11:13 -0500

    Add ST_Subdivide step into tz file generation

--- timezone_areas/generate.sh
+++ timezone_areas/generate.sh
@@ -1,18 +1,26 @@
+# run postgis in docker while doing this:
+docker run -d --rm --name tz_pg -p 5433:5433 -e POSTGRES_PASSWORD=local -e PGPORT=5433 postgis/postgis
+
 # pull the timezone areas from https://github.com/evansiroky/timezone-boundary-builder
 wget https://github.com/evansiroky/timezone-boundary-builder/releases/download/2020d/timezones-with-oceans.shapefile.zip
 unzip timezones-with-oceans.shapefile.zip
 
 # convert to pg dump
 ogr2ogr -overwrite \
-  -nln timezone_areas \
+  -nln timezone_areas_raw \
   -nlt PROMOTE_TO_MULTI \
   -nlt MULTIPOLYGON \
-  -lco CREATE_TABLE=NO \
   -lco GEOMETRY_NAME=geom \
   timezone_areas-raw.sql combined-shapefile-with-oceans.shp
 
-# remove stuff we don't need, this will leave a bunch of INSERTS (but we have to make sure it's run in a tx)
-grep -v 'standard_conforming_strings\|BEGIN\|COMMIT\|COMMENT ON TABLE' timezone_areas-raw.sql > timezone_areas.sql
+# insert into postgis
+docker exec -i tz_pg psql -U postgres < timezone_areas-raw.sql
+
+# do st_subdivide on it to make polygons faster for lookups
+docker exec -i tz_pg psql -U postgres -c "select * into timezone_areas from (select tzid, ST_Subdivide(geom, 500) as geom from timezone_areas_raw) t;"
+
+# export
+docker exec -i tz_pg pg_dump -U postgres --data-only --table timezone_areas --column-inserts > timezone_areas.sql
 
 # compress
 zstd -22 --ultra timezone_areas.sql
@@ -21,3 +29,7 @@ zstd -22 --ultra timezone_areas.sql
 rm timezones-with-oceans.shapefile.zip
 rm combined-shapefile-with-oceans*
 rm timezone_areas-raw.sql
+rm timezone_areas.sql
+
+# stop the docker container
+docker stop tz_pg
